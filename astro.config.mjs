bd33be35ff825c804f469205fe55acddb3c13089
liulang <809721414@qq.com>
2022-08-23 23:09:00 +0800

    fix: 🐛 修复打包时的问题

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  // site: '',
+  site: 'https://langiu.github.io',
   integrations: [mdx(), sitemap()],
   markdown: {
     syntaxHighlight: 'shiki',
