05ce565ebbdc8a0314324e34a4c21c5a4c9fdaec
刘浪 <ll809721414@outlook.com>
2023-03-01 23:54:45 +0800

    style: 💄 调整布局

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+// import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://langiu.github.io',
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+  ],
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
@@ -13,4 +23,8 @@ export default defineConfig({
       wrap: true,
     },
   },
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
+  // output: 'server',
 })
