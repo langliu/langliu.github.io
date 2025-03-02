49f2e68d5e6c1630524f4b0a6807cbf7c14884f3
langliu <langliu1216@gmail.com>
2025-03-02 17:46:35 +0800

    feat: 升级至tailwindcss4.x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
 import robotsTxt from 'astro-robots-txt'
 import { SITE_URL } from './src/data/config'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt(), mdx()],
+  integrations: [sitemap(), robotsTxt(), mdx()],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: 'shiki',
@@ -15,5 +15,8 @@ export default defineConfig({
       theme: 'synthwave-84',
       wrap: true
     }
+  },
+  vite: {
+    plugins: [tailwindcss()]
   }
 })
