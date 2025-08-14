aacad03a77c422603009a50f8ba804a299156085
langliu <lang.liu@heartmed.com.cn>
2025-08-14 20:54:29 +0800

    feat: 更新内容集合到V5用法

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,12 +2,15 @@ import { defineCollection, z } from 'astro:content'
 import { glob } from 'astro/loaders'
 
 const posts = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/posts' }),
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './posts' }),
   schema: z.object({
     title: z.string(),
     publishedAt: z.date(),
     description: z.string(),
     isPublish: z.boolean(),
     isDraft: z.boolean().default(false),
+    slug: z.string(),
   }),
 })
+
+export const collections = { posts }
