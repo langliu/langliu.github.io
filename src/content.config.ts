79d8456e9f6b3af517f3e3a98457245138643ed6
langliu <lang.liu@heartmed.com.cn>
2025-08-14 20:34:08 +0800

    feat: 更新获取内容集合的定义

---
+++ src/content.config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from 'astro:content'
+import { glob } from 'astro/loaders'
+
+const posts = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/posts' }),
+  schema: z.object({
+    title: z.string(),
+    publishedAt: z.date(),
+    description: z.string(),
+    isPublish: z.boolean(),
+    isDraft: z.boolean().default(false),
+  }),
+})
