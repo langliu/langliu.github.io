39f129bda495dde0ad48a359b58930accf367af7
langliu <lang.liu@heartmed.com.cn>
2025-08-15 14:22:11 +0800

    feat: ✨ 添加Sentry监控

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,31 @@ import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
 import robotsTxt from 'astro-robots-txt'
 import { SITE_URL } from './src/data/config.js'
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
+import sentry from '@sentry/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), robotsTxt(), mdx()],
+  integrations: [
+    sitemap(),
+    robotsTxt(),
+    mdx(),
+    sentry({
+      sourceMapsUploadOptions: {
+        project: 'langliu-blog',
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
       theme: 'synthwave-84',
-      wrap: true
-    }
+      wrap: true,
+    },
   },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 })
